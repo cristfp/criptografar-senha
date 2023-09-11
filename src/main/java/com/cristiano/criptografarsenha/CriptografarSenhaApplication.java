cbf2e278cb1f612f7d7f3276d413f098776d615d
cristianopinheiro <cristianopinheiro@vitai.onmicrosoft.com>
2023-09-11 15:48:39 -0300

    Initial commit

---
+++ src/main/java/com/cristiano/criptografarsenha/CriptografarSenhaApplication.java
@@ -0,0 +1,13 @@
+package com.cristiano.criptografarsenha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriptografarSenhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriptografarSenhaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cristiano/criptografarsenha/CriptografarSenhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristiano.criptografarsenha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriptografarSenhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
